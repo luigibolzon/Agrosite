c5a989a00130eb06876e23eb0751c57748c0c924
Luigi <luigibolzon@gmail.com>
2024-02-29 17:22:00 -0300

    Site iniciado, mas sem imagens pra identidade visual ainda, preciso ir atrás dos botões e logos ainda

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Banner from './Componentes/Banner/Index';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componentes/Banner/Index.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner agropecuária" />
+        </header>
+    )
+}
+
+export default Banner
